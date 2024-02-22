208df1f52e486cb537f6a4854282a16c945856dc
Andreachoros <90950613+Andreachoros@users.noreply.github.com>
2024-02-21 23:35:54 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Tabla del 4:" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << "4 x " << i << " = " << 4 * i << std::endl;
+    }
+
+    std::cout << std::endl;
+
+
+    std::cout << "Tabla del 6:" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << "6 x " << i << " = " << 6 * i << std::endl;
+    }
+
+    return 0;
+}
